f02758358f3fd5962a6c641f5a11da2943a53fda
Vitaliy Filippov <vitalif@yourcmc.ru>
2012-11-20 02:41:05 +0400

    FullLocalImage for 1.16

--- FullLocalImage.php
+++ FullLocalImage.php
@@ -4,9 +4,9 @@
  * @Author Daniel Friesen (aka: Dantman )
  * @Description Creates 2 parserfunctions. Localimage and Fullimage, they work similar
  *      to the Localurl and Fullurl functions except they return the path to a image not a article.
+ * @Author Vitaliy Filippov: ParserOutput::addImage()
  */
- 
- 
+
 $wgExtensionFunctions[] = 'wfFLImage';
 $wgHooks['LanguageGetMagic'][] = 'wfFLImageLanguageGetMagic';
 $wgExtensionCredits['parserhook'][] = array(
@@ -14,18 +14,22 @@ $wgExtensionCredits['parserhook'][] = array(
 	'author' => 'Daniel Friesen (aka: Dantman )',
 	'url' => 'http://www.mediawiki.org/wiki/Extension:Full_Local_Image'
 );
- 
+
 function wfFLImage() {
 	global $wgParser;
- 
-	$wgParser->setFunctionHook( 'Localimage', array( wgFLImageFuncts, 'Localimage' ), SFH_NO_HASH );
-	$wgParser->setFunctionHook( 'Fullimage', array( wgFLImageFuncts, 'Fullimage' ), SFH_NO_HASH );
+
+	$wgParser->setFunctionHook( 'Localimage', array( 'wgFLImageFuncts', 'Localimage' ), SFH_NO_HASH );
+	$wgParser->setFunctionHook( 'Fullimage', array( 'wgFLImageFuncts', 'Fullimage' ), SFH_NO_HASH );
 }
- 
+
 class wgFLImageFuncts {
 	function Localimage ( &$parser, $name = '', $arg = null ) {
 		$img = Image::newFromName( $name );
-		if( $img != NULL ) return $img->getURL();
+		if( $img != NULL )
+		{
+			$parser->mOutput->addImage($img->title->getDBkey());
+			return $img->getURL();
+		}
 		return '';
 	}
 	function Fullimage ( &$parser, $name = '', $arg = null ) {
@@ -34,10 +38,11 @@ class wgFLImageFuncts {
 		if( !$img ) return '';
 		$url = $img->getURL();
 		if( substr( $url, 0, strlen( $wgServer ) ) != $wgServer ) $url = $wgServer . $url;
+		$parser->mOutput->addImage($img->title->getDBkey());
 		return $url;
 	}
 }
- 
+
 # Register the magic words
 function wfFLImageLanguageGetMagic( &$magicWords,$langCode = 0 ) {
 	$magicWords['Localimage'] = array(0,'Localimage');
